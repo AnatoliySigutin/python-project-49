bba3eead2ba18f86304bea6473168b5ae8da3356
AnatoliySigutin <amoral28@mail.ru>
2025-05-03 21:15:57 +0200

    fix some files

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -5,6 +5,9 @@ import prompt
 
 def main():
     name = welcome_user()
+    calc_counter = 0
+    max_counter = 3
+
 
     x_1 = randint(1, 100)
     x_2 = randint(1, 100)
@@ -14,34 +17,39 @@ def main():
     print(f'Question: {x_1} {random_mention} {x_2}')
     answer = prompt.string('Your answer: ')
 
-    if random_mention == '+':
-        summ = x_1 + x_2
-        if summ == int(answer):
-            print('Correct!')
-        else: 
-            print(f'{answer} is wrong answer ;(. Correct answer is {summ}')
-            print("Let's try again, {name}!")
-            return
+    while calc_counter < max_counter:
+        if random_mention == '+':
+            summ = x_1 + x_2
+            if summ == int(answer):
+                print('Correct!')
+                calc_counter += 1
+            else: 
+                print(f'{answer} is wrong answer ;(. Correct answer is {summ}')
+                print("Let's try again, {name}!")
+                return
 
-    elif random_mention == '-':
-        diff = x_1 - x_2
-        if diff == int(answer):
-            print('Correct!')
-        else: 
-            print(f'{answer} is wrong answer ;(. Correct answer is {diff}')
-            print("Let's try again, {name}!")
-            return
+        elif random_mention == '-':
+            diff = x_1 - x_2
+            if diff == int(answer):
+                print('Correct!')
+                calc_counter += 1
+            else: 
+                print(f'{answer} is wrong answer ;(. Correct answer is {diff}')
+                print("Let's try again, {name}!")
+                return
 
-    elif random_mention == '*':
-        multiplication = x_1 * x_2
-        if multiplication == int(answer):
-            print('Correct!')
-        else: 
-            print(f'{answer} is wrong answer ;(. '
-      f'Correct answer is {multiplication}')
-            print("Let's try again, {name}!")
+        elif random_mention == '*':
+            multiplication = x_1 * x_2
+            if multiplication == int(answer):
+                print('Correct!')
+                calc_counter += 1
+            else: 
+                print(f'{answer} is wrong answer ;(. '
+                f'Correct answer is {multiplication}')
+                print("Let's try again, {name}!")
             return
-    
+    if calc_counter == 3:
+        print(f"Congratulations, {name}!") 
 
 
 if __name__ == '__main__':
