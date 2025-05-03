39b45355963a742ecff5e4af39e0bbacfd7c45c1
AnatoliySigutin <amoral28@mail.ru>
2025-05-03 18:51:00 +0200

    Обновил файл

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
+from brain_games.cli import welcome_user
 
 def main():
     name = welcome_user()
     
 
-    #from brain_games.cli import welcome_user
+    from brain_games.cli import welcome_user
 #from brain_games.scripts.brain_calc import brain_calc
 #from brain_games.scripts.brain_even import brain_even
 #from brain_games.scripts.brain_prime import brain_prime
