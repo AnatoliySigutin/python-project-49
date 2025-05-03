6710586f2ed1c42e088e857445f1d6c9143d8a87
AnatoliySigutin <amoral28@mail.ru>
2025-05-03 06:07:27 +0200

    Обновлены файлы README.md, brain_games/scripts/brain_games.py, pyproject.toml. Добавлена новая игра brain-progress

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,11 +2,13 @@ from brain_games.cli import welcome_user
 from brain_games.scripts.brain_even import brain_even
 from brain_games.scripts.brain_calc import brain_calc
 from brain_games.scripts.brain_gcd import brain_gcd
+from brain_games.scripts.brain_progression import brain_progression
 
 def main():
     welcome_user()
     brain_even(name)
     brain_calc(name)
+    brain_progression(name)
 
 if __name__ == '__main__':
     main()
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,41 @@
+from random import randint, choice
+from prompt_toolkit import prompt
+
+
+def brain_progression(name):
+    x_1 = randint(1, 100)
+    x_2 = randint(1, 100)
+    if x_2 <= x_1 + 10:
+        x_2 = x_1 + 15
+
+
+    list_range = []
+    x_step = randint(1, 5)
+
+    max_length = 10  
+    current = x_1
+
+    
+    while current <= x_2 and len(list_range) < max_length:
+        list_range.append(current)
+        current += x_step
+
+
+    choose_index = choice(range(len(list_range)))
+    choose_number = list_range[choose_index] 
+
+    list_range[choose_index] = '..'
+
+    print('What number is missing in the progression?')
+    print(f'Question: {list_range}')
+
+    answer = prompt('Your answer: ')
+
+    if int(answer) == choose_number:
+        print('Correct!')
+    else:
+        print(f'Wrong. The correct answer is {choose_number}.')
+        print(f"Let's try again, {name}")
+    
+
+    
