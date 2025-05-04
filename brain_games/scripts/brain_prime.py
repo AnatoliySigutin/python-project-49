518489435a8b70275440217a4eca5c8b6e1ca4f3
AnatoliySigutin <amoral28@mail.ru>
2025-05-04 07:45:21 +0200

    fix files

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -15,14 +15,15 @@ def main():
     while prime_counter < max_counter:
         number = randint(1, 100)
         print(f'Question: {number}')
-        
-        answer = prompt.string('Your answer: ')
 
-        for i in range(1, number+1):
+        answer = prompt.string('Your answer: ').lower()
+
+        division_counter = 0
+
+        for i in range(1, number + 1):
             if number % i == 0:
                 division_counter += 1
-            else:
-                division_counter = division_counter
+            
 
         if division_counter == 2 and answer == 'yes':
             print('Correct!')
