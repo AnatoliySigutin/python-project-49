1a23539a3f440cf8e7c2e0d415ec71bb183b5ca3
AnatoliySigutin <amoral28@mail.ru>
2025-05-04 07:05:41 +0200

    fix files

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,29 +1,46 @@
 from random import randint
-
+from brain_games.cli import welcome_user
 import prompt
 
 
-def brain_prime(name):
-    number = randint(1, 100)
+def main():
+    name = welcome_user()
     division_counter = 0
+    prime_counter = 0
+    max_counter = 3
 
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    print(f'Question: {number}')
-    answer = prompt.string('Your answer: ')
 
-    for i in range(number):
-        if number % i == 0:
-            division_counter += 1
-        else:
-            division_counter = division_counter
 
-    if division_counter == 2 and answer == 'yes':
-        print('Correct!')
-    elif division_counter >= 2 and answer == 'no':
-        print('Correct!')
-    elif division_counter >= 2 and answer == 'yes': 
-        print(f"{answer} is wrong answer ;(. Correct answer is 'no' ")
-        print("Let's try again, {name}!")
-    elif division_counter == 2 and answer == 'no': 
-        print(f"{answer} is wrong answer ;(. Correct answer is 'yes' ")
-        print("Let's try again, {name}!")
+    while prime_counter < max_counter:
+        number = randint(1, 100)
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+
+        for i in range(number):
+            if number % i == 0:
+                division_counter += 1
+            else:
+                division_counter = division_counter
+
+        if division_counter == 2 and answer == 'yes':
+            print('Correct!')
+
+        elif division_counter >= 2 and answer == 'no':
+            print('Correct!')
+
+        elif division_counter >= 2 and answer == 'yes': 
+            print(f"{answer} is wrong answer ;(. Correct answer is 'no' ")
+            print("Let's try again, {name}!")
+            return
+        
+        elif division_counter == 2 and answer == 'no': 
+            print(f"{answer} is wrong answer ;(. Correct answer is 'yes' ")
+            print("Let's try again, {name}!")
+            return
+        
+    print(f"Congratulations, {name}!") 
+
+
+if __name__ == '__main__':
+     main()
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,39 +1,51 @@
 from random import choice, randint
-
+from brain_games.cli import welcome_user
 import prompt
 
 
-def brain_progression(name):
-    x_1 = randint(1, 100)
-    x_2 = randint(1, 100)
-    if x_2 <= x_1 + 10:
-        x_2 = x_1 + 15
+def main():
+    name = welcome_user()
+    prog_counter = 0
+    max_counter = 3
 
-    list_range = []
-    x_step = randint(1, 5)
-
-    max_length = 10  
-    current = x_1
-
-    while current <= x_2 and len(list_range) < max_length:
-        list_range.append(current)
-        current += x_step
-
-    choose_index = choice(range(len(list_range)))
-    choose_number = list_range[choose_index] 
-
-    list_range[choose_index] = '..'
 
     print('What number is missing in the progression?')
-    print(f'Question: {list_range}')
 
-    answer = prompt.string('Your answer: ')
+    while max_counter > prog_counter:
+        x_1 = randint(1, 100)
+        x_2 = randint(1, 100)
+        if x_2 <= x_1 + 10:
+            x_2 = x_1 + 15
 
-    if int(answer) == choose_number:
-        print('Correct!')
-    else:
-        print(f'Wrong. The correct answer is {choose_number}.')
-        print(f"Let's try again, {name}")
-    
+        list_range = []
+        x_step = randint(1, 5)
 
+        max_length = 10  
+        current = x_1
+
+        while current <= x_2 and len(list_range) < max_length:
+            list_range.append(current)
+            current += x_step
+
+        choose_index = choice(range(len(list_range)))
+        choose_number = list_range[choose_index] 
+
+        list_range[choose_index] = '..'
+
+        print(f'Question: {list_range}')
+
+        answer = prompt.string('Your answer: ')
+
+        if int(answer) == choose_number:
+            print('Correct!')
+            prog_counter += 1
+        else:
+            print(f'Wrong. The correct answer is {choose_number}.')
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+if __name__ == '__main__':
+     main()
     
